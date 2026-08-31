9d37f3c9bd37db1fa4e5d66afe29d8968d4778d4
Viktor <victorholms125@gmail.com>
2026-08-31 08:49:43 +0200

    Update missing configs

--- home/.config/hypr/volume.sh
+++ home/.config/hypr/volume.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ "$1"
-
-VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
-
-notify-send -h string:x-canonical-private-synchronous:volume \
-            -h int:value:$VOL\
-            "Volume" "${VOL}%"
+swayosd-client --output-volume "$1"
